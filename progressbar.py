b2ef009aaa7abfdd64e78024d496843ab0906b0b
Emo Descontente <gabriel.pelicer.dionizio2@gmail.com>
2026-02-17 20:52:37 -0300

    Funcionamento do código melhorado, adicionado variável task_lenght para deixar o código mais leg

--- progressbar.py
+++ progressbar.py
@@ -1,4 +1,4 @@
-#  ritual de início
+# ritual de início
 print("Hello, world!")
 
 # Agora, começo do projeto PROGRESS BAR HELL YEAH
@@ -7,6 +7,8 @@ print("Progress bar: input your tasks to make your life easier!")
 # Mudar isso para input mais tarde, depois dos testes
 tasks = ["Give cat food", "Pet the panda", "Trow a ball to clippy"]
 
+tasks_lenght = len(tasks)
+
 print("Please, input your task type: ")
 print("Unique: tasks that only need complete 1 time. Ex: Feed the cat.")
 print("Various: tasks that need complete various times. Ex: Homeworks done.")
@@ -14,13 +16,15 @@ print("Time: tasks that need time. Ex: Study time.")
 # isso também
 task_type = "Unique"
 
-# Condições para o código funcionar
+tasks_compleeted = 0
 
+# Condições para o código funcionar
 if task_type == "Unique":
-    print(f"You have: {len(tasks)} tasks remain")
-    for remain in range(len(tasks)):
-        print(tasks[remain])
-    done = 0
+    task_done = 1
+    done = 2
+    tasks.pop(done - 1)
+    tasks_compleeted += task_done
+
     
 
 if task_type == "Various":
@@ -30,8 +34,12 @@ if task_type == "Time":
     print("Please, input your time needed")
 
 # Início da lógica
-tasks_compleeted = 0
-progress = int((tasks_compleeted / len(tasks)) * 100)
+progress = int((tasks_compleeted / tasks_lenght) * 100)
+
+if tasks_lenght != 0:
+    print(f"You have {tasks_lenght - 1} tasks remain:")
+    for remain in range(tasks_lenght - 1):
+        print(tasks[remain])
 
 print(f"Progress: {progress}%")
 
