864d582d0693b2d202724d6d5ef8da982e7ad210
Emo Descontente <gabriel.pelicer.dionizio2@gmail.com>
2026-02-17 21:08:20 -0300

    Mesma coisa

--- progressbar.py
+++ progressbar.py
@@ -7,7 +7,10 @@ print("Progress bar: input your tasks to make your life easier!")
 # Mudar isso para input mais tarde, depois dos testes
 tasks = ["Give cat food", "Pet the panda", "Trow a ball to clippy"]
 
-tasks_lenght = len(tasks)
+for i in tasks:
+    print(i)
+
+tasks_remaining = len(tasks)
 
 print("Please, input your task type: ")
 print("Unique: tasks that only need complete 1 time. Ex: Feed the cat.")
@@ -22,7 +25,8 @@ tasks_compleeted = 0
 if task_type == "Unique":
     task_done = 1
     done = 2
-    tasks.pop(done - 1)
+    for pop in range(done - 1):
+        tasks.pop(pop)
     tasks_compleeted += task_done
 
     
@@ -34,12 +38,14 @@ if task_type == "Time":
     print("Please, input your time needed")
 
 # Início da lógica
-progress = int((tasks_compleeted / tasks_lenght) * 100)
+tasks_lenght = len(tasks)
 
-if tasks_lenght != 0:
-    print(f"You have {tasks_lenght - 1} tasks remain:")
-    for remain in range(tasks_lenght - 1):
-        print(tasks[remain])
+progress = int((tasks_compleeted / tasks_remaining) * 100)
+
+
+print(f"You have {tasks_lenght - 1} tasks remaining:")
+for remaining in range(tasks_lenght - 1):
+        print(tasks[remaining])
 
 print(f"Progress: {progress}%")
 
