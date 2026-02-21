7b46b04e69c202ca2b3ae1a2e85ca6e9997cdff0
Emo Descontente <gabriel.pelicer.dionizio2@gmail.com>
2026-02-21 16:03:12 -0300

    Timer movido para uma pasta diferente

--- progressbar.py
+++ progressbar.py
@@ -1,20 +1,4 @@
-import time
-
-def countdown_timer(seconds):
-    while seconds > 0:
-        # Divide os segundos em minutos e segundos restantes
-        mins, secs = divmod(seconds, 60)
-        
-        # Formata o tempo como 00:00
-        timer_format = '{:02d}:{:02d}'.format(mins, secs)
-        
-        # O end='\r' faz o cursor voltar para o início da linha
-        print(timer_format, end='\r')
-        
-        time.sleep(1) # Pausa o código por 1 segundo
-        seconds -= 1
-
-
+from timer import countdown_timer
 
 # ritual de início
 print("Hello, world!")
@@ -90,6 +74,8 @@ if progress < 100:
     for remaining in range(tasks_lenght):
         num += 1    
         print(f"{num}. {tasks[remaining]}")
+
+
 
 print(f"Progress: {progress}%")
 
