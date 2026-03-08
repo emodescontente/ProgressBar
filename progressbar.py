ec6cebd5e3434da7d4d10ed04beeb00ae60121ab
Emo Descontente <gabriel.pelicer.dionizio2@gmail.com>
2026-03-08 14:43:33 -0300

    Melhoras massivas nas tasks, agora elas são um dicionário. Adicionada as defs: ShowTaskList, AddTask, NoTaskdone, CheckTask e EditTask. Todas as funções do programa foram atualizadas de acordo com essas funções. Nota: progress nãao está mais funcionando, lol

--- progressbar.py
+++ progressbar.py
@@ -9,7 +9,46 @@ def get_input():
 
 def p():
     print("")
-    return 0
+
+def ShowTaskList():
+        for t in tasks:
+            print(f"{t['id']}. [{t['type']}] {t['name']}: {t['status']}")
+
+def AddTask(type: str, task_name: str):
+    global actual_id
+    tasks.append({"id": actual_id + 1, "type": type, "name": task_name, "status": "Unfinished"})
+    actual_id += 1
+
+def NoTaskDone():
+    for t in tasks:
+        if t["status"] == "Done":
+            return False 
+    return True
+
+def CheckTask(id: int, checker: str):
+    for t in tasks:
+        if t["id"] == id:
+            match checker:
+                case "type":
+                    return t["type"]
+                case "name":
+                    return t["name"]
+                case "status":
+                    return t["status"]
+    return None
+
+def EditTask(id: int, checker: str, alt):
+    for t in tasks:
+        if t["id"] == id:
+            match checker:
+                case "type":
+                    t["type"] = alt
+                case "name":
+                    t["name"] = alt
+                case "status":
+                    t["status"] = alt
+                case "id":
+                    t["id"] = alt
 
 # ritual de início
 print("Hello, world!")
@@ -25,19 +64,25 @@ Type [clear] to delete all the tasks.
 Type [close] to end program.""")
 command_list = ["ADD", "DEL", "DONE", "PROGRESS", "CLOSE", "HELP", "CLEAR", "UNDO"]
 
+tasks = [
+    {"id": 1, "type": "S", "name": "Feed the cat", "status": "Unfinished"},
+    {"id": 2, "type": "S", "name": "Feed the dog", "status": "Done"},
+    {"id": 3, "type": "S", "name": "Feed the horse", "status": "Unfinished"},
+    {"id": 4, "type": "S", "name": "Feed the fish", "status": "Done"},
+    {"id": 5, "type": "S", "name": "Feed the baby", "status": "Done"}
+]
+actual_id = 1
 
-tasks_done = [1, 2, 3]
-tasks = ["S Cavalo", "S poney", "S Unicorno"]
 print("""
 Type [help] if you need a reminder of the commands.""")
 
 while True:
     p()
+    print("Type your command:")
     user_input = get_input().strip().upper()
 
     if user_input == "CLOSE":
         break
-
 
     if user_input not in command_list:
         p()
@@ -49,10 +94,11 @@ while True:
         print("Try start by adding tasks with [add]")
         continue
 
-    if user_input == "UNDO" and tasks_done == []:
+    if user_input == "UNDO" and NoTaskDone():
         p()
         print("No task is done, type [done] if you want mark a task as done.")
         continue
+
     match user_input:
         case "HELP":
             print("""
@@ -62,15 +108,14 @@ Type [done] and the task number when you complete one task (done a timed task wi
 Type [undo] to undo a task.
 Type [progress] when you want to check the progress done.
 Type [close] to end the program.""")
+            
         case "DONE":
-
             print("""
 Type the number of the task you want to mark as done.
-Type [end] to stop.""")
+Type [end] to stop.
+""")
 
-            for num, task in enumerate(tasks, 1):
-                status = "Done" if num in tasks_done else "Unfinished"
-                print(f"{num}. {task}: {status}")
+            ShowTaskList()
 
             while True:
                 p()
@@ -85,34 +130,29 @@ Type [end] to stop.""")
                     print("Please, type a valid number inside the tasks index.")
                     continue
 
-                if done_input in tasks_done:
+                if CheckTask(done_input, "status") == "Done":
                     p()
                     print("This task is already done.")
                     continue
 
                 if 0 < done_input <= len(tasks):
-                    tasks_done.append(done_input)
-
-                    for num, task in enumerate(tasks, 1):
-                        status = "Done" if num in tasks_done else "Unfinished"
-                        print(f"{num}. {task}: {status}")
-
+                    EditTask(done_input, "status", "Done")
                     p()
-                    print("Continue:")
-
+                    ShowTaskList()
+                    print("Continue.")
                 else:
                     p()
                     print("Please, type a valid number inside the tasks index.")
 
         case "UNDO":
+            p()
+            print("Type the number of the task you want to undo.")
+            print("Type [end] to stop.")
+            p()
+            ShowTaskList()
+            p()
+    
             while True:
-                p()
-                print("Type the number of the task you want to undo.")
-                print("Type [end] to stop.")
-                for num, task in enumerate(tasks, 1):
-                    status = "Done" if num in tasks_done else "Unfinished"
-                    print(f"{num}. {task}: {status}")
-
                 undo_input = get_input().strip().upper()
 
                 if undo_input == "END":
@@ -124,22 +164,18 @@ Type [end] to stop.""")
                     p()
                     print("That's not a number.")
                     continue
-                if undo_input not in tasks_done:
+                if CheckTask(undo_input, "status") == "Unfinished":
                     p()
                     print("That task is not done.")
                     continue
-                if 0 > undo_input > max(tasks_done):
+                if 0 > undo_input > actual_id:
                     p()
                     print("Please, type a valid number inside the index.")
                     continue
 
-                print(tasks_done)
-
-                for num, task_done in enumerate(tasks_done):
-                    if task_done == undo_input:
-                        tasks_done.pop(num)
-
-                print(tasks_done)
+                EditTask(undo_input, "status", "Unfinished")
+                print("Continue.")
+                p()
 
         case "ADD":
             description = """
@@ -149,15 +185,11 @@ Types:
 [C] CheckList: tasks that need complete various times. Example: Homeworks done.
 [T] Timed: tasks that need time. Example: Reading time
 It should be like: S Feed the cat
-Type [end] to stop inputing tasks.
-        """
+Type [end] to stop inputing tasks."""
 
             print(description)
-
-
             while True:
                 task_input = get_input()
-                p()
 
                 task_ender = task_input.strip().upper()
                 if task_ender == "END":
@@ -175,11 +207,9 @@ Type [end] to stop inputing tasks.
                     print("Please, input a valid type")
                     continue
 
-                tasks.append(parts[0] + " " + parts[1])
-                for num, i in enumerate(tasks, 1):
-                    print(f"{num}. {i}")
-                p()
-                print("Continue:")
+                AddTask(parts[0], parts[1])
+                ShowTaskList()
+                print("Continue.")
                 p()
                 continue   
             
@@ -188,13 +218,11 @@ Type [end] to stop inputing tasks.
 Type the number of the task to delete it.  
 Type [end] to stop.        
                   """) 
-            del_input = "" 
             while True:
-                for num, task in enumerate(tasks, 1):  
-                    status = "Done" if num in tasks_done else "Unfinished" 
-                    print(f"{num}. {task}: {status}")  
+                ShowTaskList() 
                 p()
                 del_input = get_input().strip().upper()
+
                 if del_input == "END":
                     break
 
@@ -207,12 +235,11 @@ Type [end] to stop.
                     continue
 
                 tasks.pop(del_input - 1)
-
-                for i, done_reduce in enumerate(tasks_done):
-                    if done_reduce > del_input:
-                        tasks_done[i] = tasks_done[i] - 1
-                    elif done_reduce == del_input:
-                        tasks_done.pop(i)
+                actual_id -= 1
+                for t in tasks:
+                    if t["id"] > del_input:
+                        t["id"] -= 1
+                print("Continue.")
 
                 if tasks == []:
                     print ("You deleted all the tasks.")
@@ -227,21 +254,11 @@ If yes, type [y], else type anyting.
 
             if clear_input == "Y":
                 tasks = []
+                actual_id = 1
                 p()
                 print("All tasks has been deleted.")
                 p()
 
         case "PROGRESS": 
-
-                p()
-
-                progress = int((len(tasks_done) / len(tasks)) * 100)
-
-                for num, task in enumerate(tasks, 1):
-                    status = "Done" if num in tasks_done else "Unfinished"
-                    print(f"{num}. {task}: {status}")
-
-                print(f"Progress: {progress}%")
-
-                if progress == 100:
-                    print("Congratulations, you did all the tasks!")
+            p()
+            ShowTaskList()
