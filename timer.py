06de010bcb6088ad68e3bee766f5887adbabcf93
Gabriel Pelicer Dionizio <gabriel.pelicer.dionizio2@gmail.com>
2026-06-01 20:16:42 -0300

    commit first again2.

---
+++ timer.py
@@ -0,0 +1,12 @@
+import time
+
+def countdown_timer(seconds):
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer_format = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer_format, end='\r')
+        time.sleep(1)
+        seconds -= 1
+if __name__ == "__main__":
+    countdown_timer(5)
+    
